1c4b94dc3e7393a4ccad3fa3ae438edacb835141
Sanjit Mohanty <msanjit@vmware.com>
2021-11-27 09:37:46 +0530

    enabling for ease debugging
    Signed-off-by: Sanjit Mohanty <msanjit@vmware.com>

---
+++ emojivoto-voting-svc/common.mk
@@ -0,0 +1,32 @@
+IMAGE_TAG ?= v12
+
+.PHONY: package protoc test
+
+target_dir := target
+
+clean:
+	rm -rf gen
+	rm -rf $(target_dir)
+	mkdir -p $(target_dir)
+	mkdir -p gen
+
+protoc:
+	protoc -I .. ../proto/*.proto --go_out=plugins=grpc:gen
+
+package: protoc compile build-container
+
+build-container:
+	docker build .. -t "asia-docker.pkg.dev/my-kubernetes-cluster-224712/asia.gcr.io/$(svc_name):$(IMAGE_TAG)" --build-arg svc_name=$(svc_name)
+
+build-multi-arch:
+	docker buildx build .. -t "asia-docker.pkg.dev/my-kubernetes-cluster-224712/asia.gcr.io/$(svc_name):$(IMAGE_TAG)" --build-arg svc_name=$(svc_name) \
+		-f ../Dockerfile-multi-arch --platform linux/amd64,linux/arm64,linux/arm/v7 --push
+
+compile:
+	GOOS=linux go build -v -o $(target_dir)/$(svc_name) cmd/server.go
+
+test:
+	go test ./...
+
+run:
+	go run cmd/server.go
